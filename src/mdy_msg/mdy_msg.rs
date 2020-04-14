947c1c7f2e0f9b60fb8831048c10d05cf4df4880
Brian Anderson <andersrb@gmail.com>
2020-04-14 03:18:05 -0600

    .

--- src/mdy_msg/mdy_msg.rs
+++ src/mdy_msg/mdy_msg.rs
@@ -3,6 +3,7 @@
 
 pub mod v1 {
     use anyhow::{Result, bail};
+    use num_enum::TryFromPrimitive;
 
     pub struct StatusByte(u8);
     pub struct DataByte(u8);
@@ -29,11 +30,60 @@ pub mod v1 {
     }
 
     impl StatusByte {
-        fn validate(&self) -> Result<()> {
-            if self.0 & (1_u8 << 7) != 0 {
-                bail!("status byte without msb");
+        pub fn validate(&self) -> Result<()> {
+            if self.0 >> 7 == 0 {
+                bail!("status byte wrong msb");
             }
             Ok(())
         }
+
+    }
+
+    pub enum Message {
+        Channel(ChannelMessage),
+        System(SystemMessage),
+    }
+
+    #[derive(Copy, Clone, TryFromPrimitive)]
+    #[repr(u8)]
+    pub enum ChannelMessage {
+        NoteOff = 0b1000,
+        NoteOn = 0b1001,
+        PolyphonicKeyPressureOrAftertouch = 0b1010,
+        ControlChangeOrChannelMode = 0b1011,
+        ProgramChange = 0b1100,
+        ChannelPressureOrAftertouch = 0b1101,
+        PitchBendChange = 0b1110,
+    }
+
+    #[derive(Copy, Clone)]
+    pub enum SystemMessage {
+        Exclusive, Common, RealTime,
+    }
+
+    impl DataByte {
+        pub fn validate(&self) -> Result<()> {
+            if self.0 >> 7 == 1 {
+                bail!("data byte wrong msb");
+            }
+            Ok(())
+        }
+    }
+
+    #[repr(u8)]
+    pub enum Mode {
+        OmniOnPoly = 1,
+        OmniOnMono = 2,
+        OmniOffPoly = 3,
+        OmniOffMono = 4,
+    }
+
+    pub enum VoiceMessageType {
+        NoteOn,
+        NoteOff,
+        ControlChange,
+        ProgramChange,
+        AfterTouch,
+        PitchBendChange,
     }
 }
